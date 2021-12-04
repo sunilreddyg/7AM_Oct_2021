f00067c6d014f35a6833bafeaf63583e3cbedf63
sunilreddyg <sunilreddy.gajjala@gmail.com>
2021-12-04 08:48:34 +0530

    Add files via upload

--- Project_197/src/mq/selenium/launchBrowsers/WebDriver_Interface.java
+++ Project_197/src/mq/selenium/launchBrowsers/WebDriver_Interface.java
@@ -1,10 +1,47 @@
 package mq.selenium.launchBrowsers;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebDriver_Interface {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		
+		String path="C:\\Users\\MINDQ\\Desktop\\new_drivers\\chromedriver.exe";
+	    System.setProperty("webdriver.chrome.driver", path);
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://facebook.com");
+		driver.manage().window().maximize();
+		
+		
+		/*
+		 * Exceptions:-->
+		 * 
+		 * 		IllegalStateException:-->
+		 * 			=> This exception we receive when don't define
+		 * 				environment path for browser drivers
+		 * 							[OR]
+		 * 
+		 * 		=> When we define wrong path  or Spell Mistakes
+		 * 
+		 * 		SessionNotCreateException:-->
+		 * 			=> When your browser driver and browser version
+		 * 				is mismatched
+		 * 
+		 * 		
+		 * 		InvalidArgumentException:-->
+		 * 				=> When url format is mismatched
+		 * 				=> Url should start with http://  or https://
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
