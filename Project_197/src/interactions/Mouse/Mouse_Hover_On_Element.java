c0931d7498b32157fd13a16a3f9b2a2aea77a3bd
sunilreddyg <sunilreddy.gajjala@gmail.com>
2021-12-13 09:18:28 +0530

    Mouse Interaction commands

---
+++ Project_197/src/interactions/Mouse/Mouse_Hover_On_Element.java
@@ -0,0 +1,32 @@
+package interactions.Mouse;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Mouse_Hover_On_Element 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\MINDQ\\Desktop\\new_drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();
+		
+		//Perform Hover action on location
+		WebElement Signin=driver.findElement(By.xpath("//span[contains(.,'Hello, Sign in')]"));
+		new Actions(driver).moveToElement(Signin).perform();
+		Thread.sleep(2000);
+		
+		//Perform Click action on location
+		WebElement Create_list=driver.findElement(By.xpath("//span[contains(.,'Create a List')]"));
+		new Actions(driver).click(Create_list).perform();
+		
+		
+	}
+
+}
