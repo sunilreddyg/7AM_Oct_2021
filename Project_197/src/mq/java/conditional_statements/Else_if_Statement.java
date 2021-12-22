ab810f89ef3bce88b166e685f5ef7989975e7227
sunilreddyg <sunilreddy.gajjala@gmail.com>
2021-12-22 09:14:01 +0530

    Conditional Statements

---
+++ Project_197/src/mq/java/conditional_statements/Else_if_Statement.java
@@ -0,0 +1,38 @@
+package mq.java.conditional_statements;
+
+public class Else_if_Statement {
+
+	public static void main(String[] args) 
+	{
+		//Multile decision statements take place here
+		String browser="chrome";
+		
+		if (browser.equals("chrome")) 
+		{
+			System.out.println("Chrome testing");
+		} 
+		else if (browser.equals("firefox"))
+		{
+			System.out.println("Firefox testing");
+		}
+		else if (browser.equals("ie")) 
+		{
+			System.out.println("ie testing");
+		}
+		else if (browser.equals("safari")) 
+		{
+			System.out.println("safari testing");
+		}
+		else 
+		{
+			System.out.println("No browser is matching");
+		}
+	 
+	 
+	 
+	 
+	 
+
+	}
+
+}
---
+++ Project_197/src/mq/java/conditional_statements/If_Statement.java
@@ -0,0 +1,35 @@
+package mq.java.conditional_statements;
+
+public class If_Statement {
+
+	public static void main(String[] args)
+	{
+		
+		
+		/*
+		 * if:-->
+		 * 		=> If only accept boolean statement.
+		 * 		=> If usefull to execute any block of statement.
+		 */
+		
+		
+		int Age=18;
+		if (Age==18)     //Here Double Equalsto return boolean value
+		{
+			System.out.println("Statement1");
+			System.out.println("Statement2");
+			
+		}
+		
+		
+		//Using If Condition only single statement to Execute
+		if(Age==18)
+			System.out.println("Statement3");
+		
+		
+		
+		
+
+	}
+
+}
